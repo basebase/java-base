046c2ece81d80b619042636e7df4afdcd51bfc70
jianfeng.li <base_2@163.com>
2022-04-19 17:11:35 +0800

    判断Line类是否为Immutable

---
+++ src/main/java/cn/xmy/base/ch03/Point.java
@@ -0,0 +1,32 @@
+package cn.xmy.base.ch03;
+
+/**
+ * @Author xiaomoyu
+ * @Date: 2022/4/19 16:47:17
+ * @Description:
+ */
+public class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
